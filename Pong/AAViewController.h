9fa8a976b6197d512d263c6435981372f1f371cf
Kyle Oba <koba@mudphone.com>
2013-11-17 23:15:29 -1000

    Basic project.

---
+++ Pong/AAViewController.h
@@ -0,0 +1,13 @@
+//
+//  AAViewController.h
+//  Pong
+//
+//  Created by Kyle Oba on 11/17/13.
+//  Copyright (c) 2013 Kyle Oba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AAViewController : UIViewController
+
+@end
---
+++ Pong/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pong
+//
+//  Created by Kyle Oba on 11/17/13.
+//  Copyright (c) 2013 Kyle Oba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AAAppDelegate class]));
+    }
+}
